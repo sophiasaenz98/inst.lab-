f64be9bdaa10bbaacc8826063df753abb3f0d028
sophiasaenz98 <sophia.saenz@ufl.edu>
2020-04-04 08:23:24 +0000

    Lab finished

---
+++ my-app/build/precache-manifest.3d3bf78dd1e5f47fc0d085cc29af3090.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "178421685d30b19384d2b35838fd77da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3060fee50b9cd3883148",
+    "url": "/static/css/main.e77cd90b.chunk.css"
+  },
+  {
+    "revision": "cc5b60f530ec9ef2959d",
+    "url": "/static/js/2.3dd64d21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3dd64d21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3060fee50b9cd3883148",
+    "url": "/static/js/main.98c90a3f.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
---
+++ my-app/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+         <p
+        >
+          Lab 8a
+        </p>
+      </header>
+       <img src="image1.png" height="150" alighItem="center"/>
+        <p>
+          Sophia Saenz
+        </p>
+    </div>
+  );
+}
+
+export default App;
